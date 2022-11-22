b2518ea315d0f9846c2ede86b64ab7cbae715102
rselimp <rselimp@gmail.com>
2022-11-22 14:13:45 +0600

    problem solved

--- src/App.js
+++ src/App.js
@@ -28,9 +28,7 @@ function App() {
       children:[
         {
           path:'/',
-          loader:async() =>{
-            return fetch('https://openapi.programming-hero.com/api/quiz')
-          },
+          
           element:<Home></Home>
         },
         {
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,27 +1,16 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
 import Banner from '../Banner/Banner';
-import Quize from '../Quize/Quize';
 
 
 
 
 const Home = () => {
-    const home = useLoaderData().data
+    
     return (
         <div>
-         <div>
-            <Banner></Banner>
-         </div>
-
-            <div className='grid gap-8 mt-8  grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
-            {
-                home.map(quize =><Quize
-                key={quize.id} 
-                quize={quize}
-                ></Quize>)
-            }
-            </div>
+           <Banner></Banner>
+          
+          
         </div>
     );
 };
